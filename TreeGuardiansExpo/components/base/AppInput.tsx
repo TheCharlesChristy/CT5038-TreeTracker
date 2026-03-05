b4f915055ad883fa285c6fc61c90e19b2e9a2b1a
FlopMeister <184410292+FlopMeister0@users.noreply.github.com>
2026-03-05 16:18:08 +0000

    Quick change

--- TreeGuardiansExpo/components/base/AppInput.tsx
+++ TreeGuardiansExpo/components/base/AppInput.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TextInput, StyleSheet, View, TextInputProps } from 'react-native';
+import { TextInput, StyleSheet, View, TextInputProps, ViewStyle } from 'react-native';
 import { Theme } from '@/styles/theme';
 
 interface AppInputProps extends TextInputProps {
