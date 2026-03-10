0ea71d7bd033b9b4e04e6c1f5f9d57d6ac6a338b
FlopMeister <184410292+FlopMeister0@users.noreply.github.com>
2026-03-10 12:43:19 +0000

    Just realised this file is not used

--- TreeGuardiansExpo/config/api.ts
+++ TreeGuardiansExpo/config/api.ts
@@ -2,7 +2,6 @@ export const API_BASE = "https://s4316157-ctxxxx.uogs.co.uk/api/";
 
 export const ENDPOINTS = {
   ADD_TREE_DATA: "add-tree-data",
-  CREATE_TREE: "create-tree",
   DB: "db",
   GET_TREES: "get-trees",
   UPLOAD_PHOTOS: "upload-photos",
